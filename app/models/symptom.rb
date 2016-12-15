5f7d0447edde161b29bc9cb864314e44d283f44b
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-15 11:54:01 +0100

    symptom validation

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,2 +1,5 @@
 class Symptom < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_inclusion_of :gender_filter, in: %w( male female both )
 end
--- spec/models/symptom_spec.rb
+++ spec/models/symptom_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Symptom, type: :model do
 
-    describe "attributes" do
-        it "should have a name" do
-            expect(Symptom.new(name:'Abdominal distension')).to be_valid
-        end
+  describe 'attributes' do
+    it 'has a name' do
+      should validate_presence_of(:name)
     end
+
+    it 'has a gender filter that is either "male", "female" or "both"' do
+      should validate_inclusion_of(:gender_filter).
+          in_array(%w(male female both))
+    end
+  end
+
 
 end
