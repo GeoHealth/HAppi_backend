9dc30be9de50d0ecc292ab879506c4b7c3656e65
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-15 13:03:45 +0100

    validation occurrence

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,4 +1,8 @@
 class Occurrence < ActiveRecord::Base
   belongs_to :symptom
   belongs_to :gps_coordinate
+
+  validates_presence_of :symptom_id
+  validates_presence_of :date
+
 end
--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Occurrence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'attributes' do
+    it { should validate_presence_of(:symptom_id) }
+
+    it { should validate_presence_of(:date) }
+
+
+  end
+
 end
