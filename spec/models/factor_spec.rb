c07035d5adf346177f1b12fad37236a6b40b4fc9
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-15 15:03:22 +0100

    factor validation

--- spec/models/factor_spec.rb
+++ spec/models/factor_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Factor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:type) }
+  end
 end
