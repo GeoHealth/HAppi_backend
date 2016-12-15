d081a99ec4e0e3929d24ea04d97d0a06456cbcf9
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-15 13:38:10 +0100

    factor instance validation

--- app/models/factor_instance.rb
+++ app/models/factor_instance.rb
@@ -1,4 +1,7 @@
 class FactorInstance < ActiveRecord::Base
   belongs_to :factor
   belongs_to :occurrence
+
+  validates_presence_of :factor_id
+  validates_presence_of :value
 end
--- spec/models/factor_instance_spec.rb
+++ spec/models/factor_instance_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe FactorInstance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'attributes' do
+    it { should validate_presence_of(:factor_id) }
+
+    it { should validate_presence_of(:value) }
+  end
 end
