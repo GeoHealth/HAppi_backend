afcd948bf0a093498aedcd119fae1451fb68813e
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-01-05 10:24:39 +0100

    define routes using RESTful notation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  scope 'occurrences' do
-    post '/' => 'occurrences#create'
-  end
+  resources :occurrences, :only => [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
