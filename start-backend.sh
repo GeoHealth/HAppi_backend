014b80c1e43715795824812c6e1e7b846f130d6a
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-01-11 22:20:29 +0100

    start backend in production mode when using the script Now it saves the PID of the rails process to the file 'rails_pid'

--- start-backend.sh
+++ start-backend.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-bundle install
-rake db:create
-rake db:migrate
-nohup rails s > backend.log &
+DEFAULT_ENVIRONMENT="development"
+ENVIRONMENT="${1:-$DEFAULT_ENVIRONMENT}"
 
+echo "Executing startup script for $ENVIRONMENT environment"
+
+echo "Installing gems"
+bundle install
+
+echo "Creating database"
+RAILS_ENV=${ENVIRONMENT} rake db:create
+
+echo "Executing migrations"
+RAILS_ENV=${ENVIRONMENT} rake db:migrate
+
+echo "Starting server in $ENVIRONMENT mode"
+nohup rails s -e ${ENVIRONMENT} > backend.log 2>&1&
+echo $! > rails_pid
