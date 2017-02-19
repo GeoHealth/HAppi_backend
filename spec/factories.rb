643fdd0a1d96005a2774fd9a42f31a9c98092004
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-02-19 12:52:23 +0100

    refactor factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,26 @@
 FactoryGirl.define do
+  # Sequences
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  # Symptoms
   factory :symptom do
+    sequence(:id)
     name 'Abdominal pain'
     gender_filter 'both'
   end
 
-  factory :occurrence do
-    symptom_id { create(:symptom).id }
-    date { Date.new }
+  # SymptomWithAverages
+  factory :symptom_with_average do
+    sequence(:id)
+    name 'pain'
+    averages []
   end
 
-  factory :occurrence_with_non_existing_symptom, class: Occurrence do
-    symptom_id -1
+  #Occurrences
+  factory :occurrence do
+    symptom_id { create(:symptom).id }
     date { Date.new }
   end
 
@@ -18,32 +28,26 @@ FactoryGirl.define do
     association :gps_coordinate, factory: :gps_coordinate, strategy: :build
   end
 
-  factory :occurrence_with_non_existing_user, class: Occurrence do
-    symptom_id { create(:symptom).id }
-    user_id -1
-    date { Date.new }
+  factory :occurrence_with_non_existing_symptom, parent: :occurrence do
+    symptom_id -1
   end
 
+  factory :occurrence_with_non_existing_user, parent: :occurrence do
+    user_id -1
+  end
+
+  # GPSCoordinates
   factory :gps_coordinate do
     latitude 50.663856999985
     longitude 4.6251496
     altitude 25.3
   end
 
-  sequence :email do |n|
-    "person#{n}@example.com"
-  end
-
+  # Users
   factory :user do
     provider 'email'
     uid { generate(:email) }
     email { uid }
     password '11112222'
-  end
-
-  factory :symptom_with_average do
-    sequence(:id)
-    name 'pain'
-    averages []
   end
 end
