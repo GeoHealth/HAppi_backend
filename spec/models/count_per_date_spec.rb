9a9c18532c91a5b8e29f0f911edc266a01835cd2
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-02-21 15:53:24 +0100

    ref #26 but we don't need to build the object ourself

--- spec/models/count_per_date_spec.rb
+++ spec/models/count_per_date_spec.rb
@@ -4,8 +4,6 @@ require 'support/have_attr_accessor'
 RSpec.describe CountPerDate do
 
   describe 'attributes' do
-    subject(:count_per_date) { build(:count_per_date) }
-
     it { should have_attr_accessor(:date) }
 
     it { should have_attr_accessor(:count) }
