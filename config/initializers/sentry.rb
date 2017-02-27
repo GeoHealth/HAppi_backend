299ac6b722d28e5b33a51593e56f27a432e8b892
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-02-27 15:52:55 +0100

    ref #24 ignore coverage on sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 # Sentry configuration for error reporting
+# :nocov:
 Raven.configure do |config|
   if ENV['SENTRY_DSN']
     config.dsn = ENV['SENTRY_DSN']
@@ -7,3 +8,4 @@ Raven.configure do |config|
       puts 'Sentry will not be active for this session because no SENTRY_DSN was given'
   end
 end
+# :nocov:
