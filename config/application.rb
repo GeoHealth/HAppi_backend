305b3c171726ae529090f312221429c2ea725dc0
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-03 18:04:54 +0100

    not assets precompilation to avoid failure: Precompiling assets failed. https://devcenter.heroku.com/articles/rails-asset-pipeline#failures-in-the-assets-precompile-task

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module HappiBackend
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Do not precompile assets
+    config.assets.initialize_on_precompile = false
+
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins '*'
