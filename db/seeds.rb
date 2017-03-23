beea2680fc9ed431cf0eb262356ec41d040ec420
Kagelmacher <alexandre.hauet@gmail.com>
2017-03-23 13:20:29 +0100

    add factors : temperature, humidity and weather_condition. add a method to import factors from json in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,20 @@ def import_symptoms
   end
 end
 
+def import_factors
+  open('./db/seeds_data/factors.json') do |factors_file|
+    factors_json = JSON.parse factors_file.read
+    factors_json.each { |factor_json|
+      factor = Factor.new(
+          name: factor_json['name'],
+          factor_type: factor_json['factor_type']
+      )
+      factor.save
+    }
+  end
+end
+
 Symptom.delete_all
 import_symptoms
+Factor.delete_all
+import_factors
