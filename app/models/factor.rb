941bd6ba931c0400ee221d8283456536d11f32bc
Kagelmacher <alexandre.hauet@gmail.com>
2017-03-23 12:59:20 +0100

    change field type of factor by factor_type

--- app/models/factor.rb
+++ app/models/factor.rb
@@ -1,4 +1,4 @@
 class Factor < ActiveRecord::Base
   validates_presence_of :name
-  validates_presence_of :type
+  validates_presence_of :factor_type
 end
--- db/migrate/20161215120320_create_factors.rb
+++ db/migrate/20161215120320_create_factors.rb
@@ -2,7 +2,7 @@ class CreateFactors < ActiveRecord::Migration
   def change
     create_table :factors do |t|
       t.string :name
-      t.string :type
+      t.string :factor_type
 
       t.timestamps null: false
     end
