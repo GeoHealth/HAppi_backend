c57ee96c4abb836f89af70fddbf0cbf663db16e0
Kagelmacher <alexandre.hauet@gmail.com>
2017-03-28 12:57:41 +0200

    test method get_closest_observation

--- spec/workers/weather_factor_instances_worker_spec.rb
+++ spec/workers/weather_factor_instances_worker_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 RSpec.describe WeatherFactorInstancesWorker, type: :worker do
-  describe '#get_weather_information' do
-    it '' do
-      expected_hash = {"date" => {"pretty" => "12:00 AM CET on March 24, 2017", "year" => "2017", "mon" => "03", "mday" => "24", "hour" => "00", "min" => "00", "tzname" => "Europe/Brussels"}, "utcdate" => {"pretty" => "11:00 PM GMT on March 23, 2017", "year" => "2017", "mon" => "03", "mday" => "23", "hour" => "23", "min" => "00", "tzname" => "UTC"}, "tempm" => "8", "tempi" => "46", "dewptm" => "4", "dewpti" => "39", "hum" => "70", "wspdm" => "14.4", "wspdi" => "8.9", "wgustm" => "", "wgusti" => "", "wdird" => "50", "wdire" => "NE", "vism" => "14", "visi" => "9", "pressurem" => "1021", "pressurei" => "30.14", "windchillm" => "-999", "windchilli" => "-999", "heatindexm" => "-9999", "heatindexi" => "-9999", "precipm" => "", "precipi" => "", "conds" => "Overcast", "icon" => "cloudy", "fog" => "0", "rain" => "0", "snow" => "0", "hail" => "0", "thunder" => "0", "tornado" => "0", "metar" => "AAXX 23231 06458 45964 /0504 10080 20041 30070 40207 51027 333 88/62 91108 91206"}
-      allow(WeatherFactorInstancesWorker.class_variable_get '@@w_api').to receive(:history_for) { {"history" => {"observations" => [expected_hash]}} }
-      result = WeatherFactorInstancesWorker.new.get_weather_information(nil, nil, Time.now)
-      expect(result).to be_instance_of Hash
-      expect(result).to eql expected_hash
+    before(:each) do
+      @instance = WeatherFactorInstancesWorker.new
     end
-  end
+    describe '#get_closest_observation' do
+        context 'when there are no observation' do
+          it 'returns nil' do
+            date = Time.now
+            observations = []
+            observation = @instance.get_closest_observation(date, observations)
+            expect(observation).to eq nil
+          end
+        end
+        context 'when there are observation' do
+          it 'returns the observation with the closest date' do
+            date = Time.parse('2012-07-11 21:00')
+            closest_observation = { "date" => {"hour" => "20", "min" => "30"}, "tempm" => "26", "hum" => "60", "conds" => "overcast"}
+            no_closest_observation = { "date" => {"hour" => "06", "min" => "30"}, "tempm" => "26", "hum" => "60", "conds" => "overcast"}
+            observations = [closest_observation, no_closest_observation]
+            observation = @instance.get_closest_observation(date, observations)
+            expect(observation).to eq closest_observation
+          end
+        end
+    end
 end
