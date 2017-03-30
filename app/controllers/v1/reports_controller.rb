0fd4e0a8d116edfac13d5b879f12b5e6d898a9a5
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-30 15:37:02 +0200

    ref #45
    /GET is OK

--- app/controllers/v1/reports_controller.rb
+++ app/controllers/v1/reports_controller.rb
@@ -13,12 +13,16 @@ class V1::ReportsController < V1::BaseController
   end
 
   def show
-    @token = params.fetch(:token)
-    @email = params.fecth(:email)
-    @report = Report.find_by_token(@token)
-    if @report.email == @email
-      render json: @report.enhanceReportWithSymptoms, :include => :symptoms
-    else
+    begin
+      @token = params.fetch(:token)
+      @email = params.fetch(:email)
+      @report = Report.find_by_token(@token)
+      if @report.expiration_date > Time.zone.now && @report.email == @email
+        render json: {report: @report.enhanceReportWithSymptoms}, :include => {:symptoms => {:include => {:occurrences => {:include => :factor_instances}}}}
+      else
+        render nothing: true, status: 404
+      end
+    rescue ActionController::ParameterMissing
       render nothing: true, status: 404
     end
   end
--- spec/controllers/v1/reports_controller_spec.rb
+++ spec/controllers/v1/reports_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 require_relative '__version__'
 
+def generate_3_occurrences_for_3_given_symptoms_for_given_user(symptoms, user, start_date)
+  occ_1_symptom_1 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date - 1.second, symptom_id: symptoms[0].id)
+  occ_1_symptom_2 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date - 1.second, symptom_id: symptoms[1].id)
+  occ_1_symptom_3 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date - 1.second, symptom_id: symptoms[2].id)
+
+  occ_2_symptom_1 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 1.second, symptom_id: symptoms[0].id)
+  occ_2_symptom_2 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 1.second, symptom_id: symptoms[1].id)
+  occ_2_symptom_3 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 1.second, symptom_id: symptoms[2].id)
+
+  occ_3_symptom_1 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 2.days, symptom_id: symptoms[0].id)
+  occ_3_symptom_2 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 2.days, symptom_id: symptoms[1].id)
+  occ_3_symptom_3 = create(:occurrence_with_3_factor_instances, user_id: user.id, date: start_date + 2.days, symptom_id: symptoms[2].id)
+  return occ_1_symptom_1, occ_1_symptom_2, occ_1_symptom_3, occ_2_symptom_1, occ_2_symptom_2, occ_2_symptom_3, occ_3_symptom_1, occ_3_symptom_2, occ_3_symptom_3
+end
+
 RSpec.describe V1::ReportsController, type: :controller do
   describe '#create' do
     it { should route(:post, @version + '/reports').to(action: :create) }
@@ -15,9 +30,10 @@ RSpec.describe V1::ReportsController, type: :controller do
         sign_in @user
       end
 
-      # start_date                                              end_date
-      #      |                                                     |
-      #      |  o1_cur_user o1_user_x o2_cur_user o2_user_x        |    o3_cur_user  o3_user_x
+      # start_date                                end_date
+      #      |                                       |
+      #      |  o1_cur_user       o2_cur_user        |    o3_cur_user
+      #      |  o1_user_x         o2_user_x          |    o3_user_x
       context 'when 3 occurrences exist for the current user, 2 included on the given interval and 3 occurrences for other users' do
         before(:each) do
           @valid_report = build(:report)
@@ -48,7 +64,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           end
 
           it 'enqueues a job to send a mail' do
-            expect{post :create, report: @valid_report.to_json}.to have_enqueued_job.on_queue('mailers')
+            expect { post :create, report: @valid_report.to_json }.to have_enqueued_job.on_queue('mailers')
           end
 
           describe 'the response' do
@@ -102,7 +118,210 @@ RSpec.describe V1::ReportsController, type: :controller do
           end
 
           it 'does not enqueue a job to send a mail' do
-            expect{post :create, report: @invalid_report.to_json}.not_to have_enqueued_job.on_queue('mailers')
+            expect { post :create, report: @invalid_report.to_json }.not_to have_enqueued_job.on_queue('mailers')
+          end
+        end
+      end
+    end
+  end
+
+  describe '#show' do
+    it { should route(:get, @version + '/report').to(action: :show) }
+
+    context 'when no user is logged in' do
+      it_behaves_like 'GET protected with authentication controller', :show
+    end
+
+    context 'when a user is logged in' do
+      before(:each) do
+        @user = AuthenticationTestHelper.set_valid_authentication_headers(@request)
+        sign_in @user
+      end
+
+      #                  start_date                                       end_date
+      #-----------------------|----------------------------------------------|---------
+      #   occ_1_symptom_1     |      occ_2_symptom_1      occ_3_symptom_1    |
+      #   occ_1_symptom_2     |      occ_2_symptom_2      occ_3_symptom_2    |
+      #-----------------------|----------------------------------------------|---------
+      #   occ_1_symptom_3     |      occ_2_symptom_3      occ_3_symptom_3    | <= those are not include in the report
+      #-----------------------|----------------------------------------------|---------
+      context 'when the logged in user has 3 occurrences of 3 symptoms and has created a report for the 2 latest occurrences of only 2 symptoms' do
+        before(:each) do
+          @start_date = Time.parse('2017-07-11 1:00')
+          @end_date = @start_date + 1.week
+          @expiration_date = @end_date + 2.weeks
+
+          @symptoms = create_list(:symptom, 3)
+          @occ_1_symptom_1, @occ_1_symptom_2, @occ_1_symptom_3, @occ_2_symptom_1, @occ_2_symptom_2, @occ_2_symptom_3, @occ_3_symptom_1, @occ_3_symptom_2, @occ_3_symptom_3 = generate_3_occurrences_for_3_given_symptoms_for_given_user(@symptoms, @user, @start_date)
+
+          @report = create(:report, user_id: @user.id, start_date: @start_date, end_date: @end_date, expiration_date: @expiration_date)
+          @report.occurrences << [@occ_2_symptom_1, @occ_2_symptom_2, @occ_3_symptom_1, @occ_3_symptom_2]
+        end
+
+        context 'when a second user has similar occurrences and similar report' do
+          before(:each) do
+            second_user = create(:user)
+            occ_1_symptom_1, occ_1_symptom_2, occ_1_symptom_3, occ_2_symptom_1, occ_2_symptom_2, occ_2_symptom_3, occ_3_symptom_1, occ_3_symptom_2, occ_3_symptom_3 = generate_3_occurrences_for_3_given_symptoms_for_given_user(@symptoms, second_user, @start_date)
+
+            second_report = create(:report, user_id: second_user.id, start_date: @start_date, end_date: @end_date, expiration_date: @expiration_date)
+            second_report.occurrences << [occ_2_symptom_1, occ_2_symptom_2, occ_3_symptom_1, occ_3_symptom_2]
+          end
+
+          context 'when the given token and email are valid and associated to the same report' do
+            before(:each) do
+              @token = @report.token
+              @email = @report.email
+            end
+
+            context 'when expiration_date is not passed yet' do
+              before(:each) do
+                @report.expiration_date = Time.now + 2.weeks
+                @report.save
+              end
+
+              before(:each) do
+                get :show, token: @token, email: @email
+              end
+
+              it 'responds with 200' do
+                is_expected.to respond_with 200
+              end
+
+              it 'returns a JSON' do
+                expect(response.body).to be_instance_of String
+                expect { JSON.parse(response.body) }.not_to raise_exception
+              end
+
+              describe 'the response' do
+                subject { JSON.parse(response.body) }
+
+                it 'contains a key "report"' do
+                  expect(subject).to have_key 'report'
+                end
+
+                describe 'the report' do
+                  subject { JSON.parse(response.body)['report'] }
+
+                  it 'has an id, user_id, start_date, end_date, email, token, expiration_date, symptoms' do
+                    expect(subject).to have_key 'id'
+                    expect(subject).to have_key 'user_id'
+                    expect(subject).to have_key 'start_date'
+                    expect(subject).to have_key 'end_date'
+                    expect(subject).to have_key 'email'
+                    expect(subject).to have_key 'token'
+                    expect(subject).to have_key 'expiration_date'
+                    expect(subject).to have_key 'symptoms'
+                  end
+
+                  it 'has not the key "occurrences"' do
+                    expect(subject).not_to have_key 'occurrences'
+                  end
+
+                  it 'has the same token as the one given in parameter' do
+                    expect(subject['token']).to eq @token
+                  end
+
+                  it 'has the same email as the one given in parameter' do
+                    expect(subject['email']).to eq @email
+                  end
+
+                  describe 'the symptoms array' do
+                    subject { JSON.parse(response.body)['report']['symptoms'] }
+
+                    it 'contains 2 symptoms' do
+                      expect(subject.length).to eq 2
+                    end
+
+                    it 'contains the symptom 1 and the symptom 2' do
+                      expected_ids = [@symptoms[0].id, @symptoms[1].id]
+                      expect(expected_ids).to include subject[0]['id']
+                      expect(expected_ids).to include subject[1]['id']
+                    end
+
+                    it 'has 2 occurrences for each symptom' do
+                      subject.each do |symptom|
+                        expect(symptom['occurrences'].length).to eq 2
+                      end
+                    end
+
+                    it 'has occ_2_symptom_1 and occ_3_symptom_1 for the first symptom and occ_2_symptom_2 and occ_3_symptom_2 for the second symptom' do
+                      expected_ids_symptom_1 = [@occ_2_symptom_1.id, @occ_3_symptom_1.id]
+                      expect(expected_ids_symptom_1).to include subject[0]['occurrences'][0]['id']
+                      expect(expected_ids_symptom_1).to include subject[0]['occurrences'][1]['id']
+                      expected_ids_symptom_2 = [@occ_2_symptom_2.id, @occ_3_symptom_2.id]
+                      expect(expected_ids_symptom_2).to include subject[1]['occurrences'][0]['id']
+                      expect(expected_ids_symptom_2).to include subject[1]['occurrences'][1]['id']
+                    end
+
+                    it 'has 3 factor_instances for each occurrence' do
+                      subject.each do |symptom|
+                        symptom['occurrences'].each do |occurrence|
+                          expect(occurrence['factor_instances'].length).to eq 3
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+
+            context 'when expiration_date is passed' do
+              before(:each) do
+                @report.expiration_date = Time.parse('2012-07-11 21:00')
+                @report.save
+              end
+
+              before(:each) do
+                get :show, token: @token, email: @email
+              end
+
+              it 'responds with status 404' do
+                is_expected.to respond_with 404
+              end
+            end
+          end
+
+          context 'when the given token is valid but the given email is not associated to this token' do
+            before(:each) do
+              @token = @report.token
+              @email = @report.email + 'something is wrong'
+            end
+
+            before(:each) do
+              get :show, token: @token, email: @email
+            end
+
+            it 'responds with status 404' do
+              is_expected.to respond_with 404
+            end
+          end
+
+          context 'when no email is given as parameter' do
+            before(:each) do
+              @token = @report.token
+            end
+
+            before(:each) do
+              get :show, token: @token
+            end
+
+            it 'responds with status 404' do
+              is_expected.to respond_with 404
+            end
+          end
+
+          context 'when no token is given' do
+            before(:each) do
+              @email = @report.email
+            end
+
+            before(:each) do
+              get :show, email: @email
+            end
+
+            it 'responds with status 404' do
+              is_expected.to respond_with 404
+            end
           end
         end
       end
