9c162d4a0a3c52b075661e826b1178de7c913ffa
Kagelmacher <alexandre.hauet@gmail.com>
2017-04-10 14:05:09 +0200

    remove a print

--- app/workers/elasticsearch_worker.rb
+++ app/workers/elasticsearch_worker.rb
@@ -5,7 +5,6 @@ class ElasticsearchWorker
     occurrence = Occurrence.find(occurrence_id)
     if (occurrence)
       uri = URI(ENV['ELASTIC_URL'] + (occurrence.id).to_s)
-      puts uri
       req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
       req.body = occurrence.to_json
       res = Net::HTTP.start(uri.hostname, uri.port) do |http|
