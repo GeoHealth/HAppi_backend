e0512c0b02f08a2e24f1d657fa744fd12db35f0c
Kagelmacher <alexandre.hauet@gmail.com>
2017-04-10 18:01:08 +0200

    add verification of the env var

--- app/workers/elasticsearch_worker.rb
+++ app/workers/elasticsearch_worker.rb
@@ -1,16 +1,24 @@
 class ElasticsearchWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => 2
 
   def perform(occurrence_id)
     occurrence = Occurrence.find(occurrence_id)
     if (occurrence)
+      raise(Exception, 'No ELASTIC_URL found') if not ENV['ELASTIC_URL']
       uri = URI(ENV['ELASTIC_URL'] + (occurrence.id).to_s)
       req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-      req.body = occurrence.to_json
+      req.body = build_json(occurrence)
       res = Net::HTTP.start(uri.hostname, uri.port) do |http|
         http.request(req)
       end
     end
     res
   end
+
+  def build_json(occurrence)
+    result = JSON::parse(occurrence.to_json).merge('location' => "#{occurrence.gps_coordinate.latitude}, #{occurrence.gps_coordinate.longitude}")
+    result.to_json
+  end
+
 end
--- spec/workers/elasticsearch_worker_spec.rb
+++ spec/workers/elasticsearch_worker_spec.rb
@@ -2,27 +2,39 @@ require 'rails_helper'
 RSpec.describe ElasticsearchWorker, type: :worker do
 
   describe '#perform' do
-    before(:each) do
-      @e = ElasticsearchWorker.new
-    end
-    context 'when there are no occurrence in db' do
-      it 'raise an exception ActiveRecord::RecordNotFound' do
-        assert_raises(ActiveRecord::RecordNotFound) do
-          @e.perform(1)
-        end
-      end
-    end
-    context 'when there are occurrence in db' do
+    context 'when there is a ELASTIC_URL' do
       before(:each) do
-        @occurrence = create(:occurrence_with_gps_coordinates)
+        @e = ElasticsearchWorker.new
       end
-      context 'when http post is done' do
-        before(:each) do
-          @url = ENV['ELASTIC_URL'] + @occurrence.id.to_s
-          stub_request(:any, @url).to_return(status: 202)
+      context 'when there are no occurrence in db' do
+        it 'raise an exception ActiveRecord::RecordNotFound' do
+          assert_raises(ActiveRecord::RecordNotFound) do
+            @e.perform(1)
+          end
         end
-        it 'returns code 202' do
-          expect(@e.perform(@occurrence.id).code).to eq ("202")
+      end
+      context 'when there are occurrence in db' do
+        before(:each) do
+          @occurrence = create(:occurrence_with_gps_coordinates)
+        end
+        context 'when http post is done' do
+          before(:each) do
+            @url = ENV['ELASTIC_URL'] + @occurrence.id.to_s
+            stub_request(:any, @url).to_return(status: 202)
+          end
+          it 'returns code 202' do
+            expect(@e.perform(@occurrence.id).code).to eq ("202")
+          end
+        end
+      end
+    end
+    context 'when there is no ELASTIC_URL' do
+      before(:each) do
+        ENV['ELASTIC_URL'] = nil
+      end
+      it 'raise an exception ' do
+        assert_raises(Exception) do
+          @e.perform(1)
         end
       end
     end
