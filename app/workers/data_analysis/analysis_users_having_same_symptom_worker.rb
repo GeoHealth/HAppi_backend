b093b859cc2b0b39cd656a9ea54d58262fc540fe
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-13 17:35:58 +0200

    ref #52 & #53 filter occurrences

--- app/workers/data_analysis/analysis_users_having_same_symptom_worker.rb
+++ app/workers/data_analysis/analysis_users_having_same_symptom_worker.rb
@@ -1,6 +1,8 @@
 class DataAnalysis::AnalysisUsersHavingSameSymptomWorker
   include Sidekiq::Worker
 
+  @@bin_lcm_path = './data-analysis-fimi03'
+
   def perform(analysis_id)
     @analysis = DataAnalysis::AnalysisUsersHavingSameSymptom.find analysis_id
     create_input_file @analysis
@@ -14,7 +16,21 @@ class DataAnalysis::AnalysisUsersHavingSameSymptomWorker
   end
 
   def create_input_file(analysis)
-    # code here
+    input_path = "#{@@bin_lcm_path}/inputs/#{analysis.token}.input"
+    system("touch #{input_path}")
+
+    occurrences = get_occurrences_matching_analysis analysis
+
+
+    system("echo content >> #{input_path}")
+  end
+
+  def get_occurrences_matching_analysis(analysis)
+    Occurrence
+        .select('DISTINCT user_id, symptom_id')
+        .where('date BETWEEN :start_date AND :end_date',
+               {start_date: analysis.start_date, end_date: analysis.end_date})
+        .order(:user_id)
   end
 
   def delete_input_file(analysis)
--- spec/workers/data_analysis/analysis_users_having_same_symptom_worker_spec.rb
+++ spec/workers/data_analysis/analysis_users_having_same_symptom_worker_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe DataAnalysis::AnalysisUsersHavingSameSymptomWorker, type: :worker
       @system_return = true
       @delete_input_file_return = true
 
-      allow(@job).to receive(:create_input_file) { @create_input_file_return }
-      allow(@job).to receive(:system).with("./data-analysis-fimi03/fim_closed ./data-analysis-fimi03/inputs/#{@analysis.token}.input #{@analysis.threshold} ./data-analysis-fimi03/outputs/#{@analysis.token}.output") { @system_return }
-      allow(@job).to receive(:delete_input_file) { @delete_input_file_return }
+      allow(@job).to receive(:create_input_file) {@create_input_file_return}
+      allow(@job).to receive(:system).with("./data-analysis-fimi03/fim_closed ./data-analysis-fimi03/inputs/#{@analysis.token}.input #{@analysis.threshold} ./data-analysis-fimi03/outputs/#{@analysis.token}.output") {@system_return}
+      allow(@job).to receive(:delete_input_file) {@delete_input_file_return}
     end
 
     it 'makes a call to create_input_file' do
@@ -60,14 +60,84 @@ RSpec.describe DataAnalysis::AnalysisUsersHavingSameSymptomWorker, type: :worker
     end
   end
 
-  describe '#create_input_file' do
-    context 'when there are 5 users in the database having 3 symptoms between the start_date and end_date of the report' do
+  describe '#get_occurrences_matching_analysis' do
+    #                  start_date                                       end_date
+    #-----------------------|----------------------------------------------|---------
+    #    occ1_s1 occ2_s1    |      occ_1_symptom_1      occ_2_symptom_1    | <= each user have those occurrences
+    #        occ1_s2        |                 occ_1_symptom_2              |
+    #-----------------------|----------------------------------------------|---------
+    context 'when there are 5 users in the database having 3 occurrences (2 of the same symptom and 1 other) between the start_date and end_date of the report and 3 occurrences outside this range' do
       before(:each) do
-
+        @users = create_list(:user, 5)
+        @symptoms = create_list(:symptom, 2)
+        @users.each do |user|
+          create_list(:occurrence, 2, user_id: user.id, symptom_id: @symptoms[0].id, date: @analysis.start_date + 1.hour)
+          create_list(:occurrence, 1, user_id: user.id, symptom_id: @symptoms[1].id, date: @analysis.start_date + 1.hour)
+          create_list(:occurrence, 2, user_id: user.id, symptom_id: @symptoms[0].id, date: @analysis.start_date - 1.day)
+          create_list(:occurrence, 1, user_id: user.id, symptom_id: @symptoms[1].id, date: @analysis.start_date - 1.day)
+        end
       end
 
-      it 'creates a file containing' do
+      subject {@job.get_occurrences_matching_analysis @analysis}
 
+      it 'returns a list of 10 occurrences' do
+        expect(subject.length).to eq 10
+      end
+
+      it 'returns a list with the 5 ids of users' do
+        users_ids = []
+        subject.each do |occurrrence|
+          users_ids << occurrrence.user_id
+        end
+
+        @users.each do |user|
+          expect(users_ids).to include user.id
+        end
+      end
+
+      it 'returns a list with the ids of the 2 symptoms' do
+        symptoms_ids = []
+        subject.each do |occurrence|
+          symptoms_ids << occurrence.symptom_id
+        end
+
+        @symptoms.each do |symptom|
+          expect(symptoms_ids).to include symptom.id
+        end
+      end
+
+      it 'returns a list ordered by user_id' do
+        expected_users_id = []
+        @users.each do |user|
+          expected_users_id << user.id
+        end
+        expected_users_id.sort
+
+        expected_users_id.each_with_index do |user_id, index|
+          expect(subject[index * 2].user_id).to eq user_id
+          expect(subject[index * 2 + 1].user_id).to eq user_id
+        end
+      end
+    end
+  end
+
+  describe '#create_input_file' do
+    context 'when there are 5 users in the database having 3 occurrences between the start_date and end_date of the report' do
+      before(:each) do
+        @users = create_list(:user, 5)
+        @users.each do |user|
+          create_list(:occurrence, 3, date: @analysis.start_date + 1.hour)
+          create_list(:occurrence, 3, date: @analysis.start_date - 1.day)
+        end
+      end
+
+      before(:each) do
+        allow(@job).to receive(:system)
+      end
+
+      it 'creates a file containing by calling "system touch token.input"' do
+        expect(@job).to receive(:system).with("touch ./data-analysis-fimi03/inputs/#{@analysis.token}.input")
+        @job.create_input_file @analysis
       end
     end
   end
